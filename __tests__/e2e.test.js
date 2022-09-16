62a4511d9d08e211af8f236790d783fa536ac535
Chris Wong <ctcwong73@gmail.com>
2022-09-16 13:17:48 -0700

    Update e2e test comments

--- __tests__/e2e.test.js
+++ __tests__/e2e.test.js
@@ -4,23 +4,11 @@ import LoginPage from '../client/components/LoginPage.jsx';
 import PlaylistForm from '../client/components/PlaylistForm.jsx';
 import 'jsdom-global/register';
 import { render, screen, cleanup, fireEvent } from '@testing-library/react';
-// const render = ReactDOM.render;
 const puppeteer = require('puppeteer');
 
 describe("Test React Login Page", () => {
     let browser;
     let page;
-
-    // beforeAll(async () =>{
-    //     browser = await puppeteer.launch();
-    //     page = await browser.newPage();
-        
-    // });
-
-    // it('should be titled "Axolotl Beats"', async () => {
-    //     await page.goto("http://localhost:8080");
-    //     await expect(page.title()).resolves.toMatch('Axolotl Beats');
-    // })
 
     it('should render a login button', () =>{
         render(<LoginPage />)
@@ -35,18 +23,18 @@ describe("Test React Login Page", () => {
     
 });
 
-// xdescribe("PlaylistForm.jsx", () =>{
-//     it('should render a form with class "playlist-form"', () =>{
-//         render(<PlaylistForm />)
-//         const playlistForm = screen.getByRole('form', {name:/playlist-form/i})
-//         expect(playlistForm.getAttribute('class')).toEqual('playlist-form')
-//     })
+xdescribe("PlaylistForm.jsx", () =>{
+    it('should render a form with class "playlist-form"', () =>{
+        render(<PlaylistForm />)
+        const playlistForm = screen.getByRole('form', {name:/playlist-form/i})
+        expect(playlistForm.getAttribute('class')).toEqual('playlist-form')
+    })
    
-//     it('should submit the form', () =>{
-//         const {container} = render(<PlaylistForm />)
+    it('should submit the form', () =>{
+        const {container} = render(<PlaylistForm />)
         
-//     })
-// })
+    })
+})
 
 
 
